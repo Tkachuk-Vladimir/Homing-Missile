2e20c5aa370fb8113132f4a90bd04b4ff4b58d41
vovatk <tck.vova@ya.ru>
2021-09-23 15:51:49 +0300

    add files homing missile

---
+++ Assets/RocetScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody2D))]
+public class RocetScript : MonoBehaviour
+{
+    public Transform target;
+    public float rocetSpeed = 5f;
+    public float rocetRotateSpeed = 200f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.velocity = transform.up * rocetSpeed;
+
+        Vector2 direction = (Vector2)target.position - rb.position; // находим вектор до цели
+
+        direction.Normalize();// вычисляем нормаль к вектору direction
+
+        float rotateAmount = Vector3.Cross(direction, transform.up).z; // Вычисляем угол между ракетой и лет.тарелкой
+
+        rb.angularVelocity = -rotateAmount * rocetRotateSpeed; // з
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/SpaceshipV2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipV2 : MonoBehaviour
+{
+
+    public float speedShip = 1f;
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speedShip * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speedShip * Time.deltaTime;
+
+        transform.Translate(horizontal, vertical,0);
+    }
+
+}
